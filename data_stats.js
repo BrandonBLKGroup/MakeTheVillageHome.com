29a2effd0baf28945ab0b0363bc852720d33a1b2
Jarvis - BLK Group <JarvisBLKGroup@Gmail.com>
2026-03-01 02:05:13 +0000

    MakeTheVillageHome.com - Hot Springs Village off-market site
    4,304 homes mapped across 145 subdivisions
    Vacant lots filtered (11,937 excluded)
    Buy/sell forms with Supabase + Google Sheets
    Dark map with pulse markers
    Full SEO + structured data

---
+++ data_stats.js
@@ -0,0 +1,129 @@
+const HSV_STATS = {
+  "totalParcels": 4304,
+  "totalSubdivisions": 145,
+  "totalAssessedValue": 1539512615.0,
+  "avgAssessedValue": 357693,
+  "generated": "2026-03-01 01:12 UTC",
+  "topSubdivisions": [
+    {
+      "name": "VEREDA",
+      "count": 243
+    },
+    {
+      "name": "ISABELLA",
+      "count": 215
+    },
+    {
+      "name": "LADERA",
+      "count": 214
+    },
+    {
+      "name": "DIAMANTE",
+      "count": 190
+    },
+    {
+      "name": "CIELO",
+      "count": 172
+    },
+    {
+      "name": "MARIA",
+      "count": 144
+    },
+    {
+      "name": "ALTA VISTA",
+      "count": 113
+    },
+    {
+      "name": "PROMESA",
+      "count": 89
+    },
+    {
+      "name": "REATA",
+      "count": 81
+    },
+    {
+      "name": "SERGIO",
+      "count": 81
+    },
+    {
+      "name": "ESCOCIA",
+      "count": 81
+    },
+    {
+      "name": "RESPLANDOR",
+      "count": 80
+    },
+    {
+      "name": "ENCANTADO",
+      "count": 70
+    },
+    {
+      "name": "ADORACION",
+      "count": 70
+    },
+    {
+      "name": "CARIBE",
+      "count": 69
+    },
+    {
+      "name": "ZAFIRO",
+      "count": 68
+    },
+    {
+      "name": "RONQUILLO",
+      "count": 66
+    },
+    {
+      "name": "GALEON",
+      "count": 65
+    },
+    {
+      "name": "MURILLO",
+      "count": 61
+    },
+    {
+      "name": "SACEDON",
+      "count": 59
+    },
+    {
+      "name": "SANCHEZ",
+      "count": 54
+    },
+    {
+      "name": "SAN PABLO",
+      "count": 53
+    },
+    {
+      "name": "FAISAN",
+      "count": 52
+    },
+    {
+      "name": "CORONADO",
+      "count": 52
+    },
+    {
+      "name": "MARINERO",
+      "count": 49
+    },
+    {
+      "name": "SORPRESA",
+      "count": 49
+    },
+    {
+      "name": "ALEGRIA",
+      "count": 48
+    },
+    {
+      "name": "BELLISIMO",
+      "count": 47
+    },
+    {
+      "name": "BALLESTEROS",
+      "count": 46
+    },
+    {
+      "name": "PONCE DE LEON",
+      "count": 45
+    }
+  ]
+};
